a2dbb1a69bf0beb2fb5f93a7305cc1a8594578a1
vitroid <vitroid@gmail.com>
2025-04-28 13:55:47 +0900

    Figure4.pyのプロット関数を整理し、引数名を明確にしました。また、変数の命名規則をコメントとして追加し、コードの可読性と再利用性を向上させました。メインブロックを追加し、プロットの表示とPDF保存を行うようにしました。

--- Figure4.py
+++ Figure4.py
@@ -32,68 +32,18 @@ def determine_stable_phase(mu_e, Deltamu, structures):
     return stmin
 
 
-# Nomenclature for variables
-# f, mu: free energy/chemical potential
-# h    : enthalpy
-#
-# _w   : of water
-# _i   : of ice
-# _e   : of the empty lattice
-# _f   : of the filled lattice
-# _g   : of the guest
-# _c   : in the cage
-#
+"""
+Nomenclature for variables
+f, mu: free energy/chemical potential
+h    : enthalpy
 
-
-# def MultipleClathrate(gases, pressures, temperatures, structures):
-#     beta = 1 / (NkB * temperatures)
-#     f = []
-#     mu = []
-#     for gas, pressure in zip(gases, pressures):
-#         if pressure > 0.0:
-#             mu.append(
-#                 chempot.chempot(temperatures, pressure) +
-#                 chempot.IntegrationFixMinus(temperatures, dimen=0))
-#             ff = dict()
-#             for cage, R in radii.items():
-#                 # sigma and epsilon must be the intermolecular ones.
-#                 ff[cage] = fvalue({R: nmemb[cage]}, gas.sig,
-#                                   gas.epsK * 8.314 / 1000, beta)
-#             f.append(ff)
-#     Deltamu = vdWP.ChemPotByOccupation(temperatures, f, mu, structures)
-
-#     X = Deltamu["CS1"] - Deltamu["HS1"]
-#     Y = Deltamu["CS2"] - Deltamu["HS1"]
-#     return X, Y
-
-
-# User variables
-pressure = 101325.00 * 50  # Pa
-temperatures = 273.15
-beta = 1.0 / (NkB * temperatures)
-
-LJ = namedtuple("LJ", ["sig", "epsK"])
-
-# guest = "Methane"
-structures = ["CS2", "CS1", "HS1", "TS1"]  # , "TS1", "HS1"]
-
-####### chemical potential of gas ######################################
-stericterm = chempot.molecular_chemical_potential_corrections(
-    temperatures, mass=1.0, symmetry_number=1, moment_of_inertia=(0, 0, 0)
-)
-
-mu_g = (
-    chempot.ideal_gas_chemical_potential(temperatures, pressure)
-    + chempot.phase_space_integration_correction(temperatures, degrees_of_freedom=0)
-    + stericterm
-)
-
-####### structure-dependent terms ######################################
-
-mu_e = crystals.mu_e
-
-plt.rcParams["font.size"] = 14
-plt.rcParams["font.family"] = "sans-serif"
+_w   : of water
+_i   : of ice
+_e   : of the empty lattice
+_f   : of the filled lattice
+_g   : of the guest
+_c   : in the cage
+"""
 
 
 def calculate_mixed_clathrate_phase(
@@ -101,18 +51,21 @@ def calculate_mixed_clathrate_phase(
     g2,
     beta,
     pressure,
+    temperatures,
+    mu_e,
+    structures,
     composition_ratios=np.linspace(0.0, 1.0, 100),
     target_phase=None,
 ):
     f1 = dict()
     f2 = dict()
-    for cage, R in crystals.radii.items():
+    for cage, radius in crystals.radii.items():
         # sigma and epsilon must be the intermolecular ones.
         f1[cage] = fvalue(
-            {R: crystals.nmemb[cage]}, g1.sig, g1.epsK * 8.314 / 1000, beta
+            {radius: crystals.nmemb[cage]}, g1.sig, g1.epsK * 8.314 / 1000, beta
         )
         f2[cage] = fvalue(
-            {R: crystals.nmemb[cage]}, g2.sig, g2.epsK * 8.314 / 1000, beta
+            {radius: crystals.nmemb[cage]}, g2.sig, g2.epsK * 8.314 / 1000, beta
         )
 
     phases = []
@@ -168,7 +121,7 @@ def plot_phase_marker(sig, epsK, phases, lastphase, ax):
         color = "#88f"
     else:
         color = "brown"
-    # I-III-Iの場合を区別して描く。(実際には見付かっていない)
+    # Distinguish I-III-I case (not actually found)
     if lastphase == "CS1" and "TS1" in phases and len(phases) == 2:
         ax.plot(sig, epsK, color="orange", marker=".")
     else:
@@ -228,176 +181,294 @@ def plot_phase_contours(ax, X, Y, Z, Z2):
         collection.set_edgecolor("white")
 
 
-fig, axes = plt.subplots(
-    nrows=1, ncols=3, figsize=(12, 5), sharey=True, gridspec_kw={"wspace": 0}
-)
+def plot_phase_diagram_Figure4a(
+    ax,
+    temperatures,
+    pressure,
+    beta,
+    mu_e,
+    structures,
+    inter,
+    tip4pice,
+    gases,
+    composition_ratios,
+    xyticks,
+):
+    # (a)
+    # Calculate the minimum concentration that, when mixed with Q, produces II.
+    guest = "Br2"
+    ax.set_xlabel(r"$\sigma_g / \AA$")
+    ax.set_ylabel(r"$\epsilon_g / K$")
+    ax.set_xlim(3.45, 5.1)
+    ax.set_ylim(120.0, 600)
+    x = np.linspace(3.45, 5.1, xyticks * 2)
+    y = np.linspace(120.0, 600, xyticks * 2)
+    X, Y = np.meshgrid(x, y)
+    Z = np.zeros_like(X)
+    for ix, sig in enumerate(x):
+        for iy, epsK in enumerate(y):
+            inter2 = LJ(
+                sig=(tip4pice.sig + sig) / 2, epsK=(tip4pice.epsK * epsK) ** 0.5
+            )
+            phases, lastphase = calculate_mixed_clathrate_phase(
+                inter2,
+                inter[guest],
+                beta,
+                pressure,
+                temperatures,
+                mu_e,
+                structures,
+                composition_ratios=composition_ratios,
+                target_phase="CS2",
+            )
+            z = 1.0
+            for i, phase in enumerate(phases):
+                if phase == "CS2":
+                    z = composition_ratios[i]
+                    break
+            Z[iy, ix] = z
 
-xyticks = 40
+    contours = ax.contour(
+        X, Y, Z, levels=[0.0001, 0.001, 0.01, 0.1], colors="black", linewidths=1
+    )
+    ax.clabel(contours, inline=True, fontsize=8)
+    contours = ax.contour(
+        X,
+        Y,
+        Z,
+        levels=[
+            0.9999,
+        ],
+        colors="black",
+        linestyles="dashed",
+    )
 
-# (a)
-# Calculate the minimum concentration that, when mixed with Q, produces II.
-ax = axes[0]
-guest = "Br2"
-ax.set_xlabel(r"$\sigma_g / \AA$")
-ax.set_ylabel(r"$\epsilon_g / K$")
-ax.set_xlim(3.45, 5.1)
-ax.set_ylim(120.0, 600)
-composition_ratios = np.concatenate(
-    [np.array([0.0]), np.logspace(-4, 0.0, 300)]
-)  # 1e-4 .. 1e0
-x = np.linspace(3.45, 5.1, xyticks * 2)
-y = np.linspace(120.0, 600, xyticks * 2)
-X, Y = np.meshgrid(x, y)
-Z = np.zeros_like(X)
-for ix, sig in enumerate(x):
-    for iy, epsK in enumerate(y):
-        inter2 = LJ(sig=(tip4pice.sig + sig) / 2, epsK=(tip4pice.epsK * epsK) ** 0.5)
-        phases, lastphase = calculate_mixed_clathrate_phase(
-            inter2,
-            inter[guest],
-            beta,
-            pressure,
-            composition_ratios=composition_ratios,
-            target_phase="CS2",
-        )
-        z = 1.0
-        for i, phase in enumerate(phases):
-            if phase == "CS2":
-                z = composition_ratios[i]
-                break
-        Z[iy, ix] = z
+    contours = ax.contour(
+        X,
+        Y,
+        Z,
+        levels=[
+            0.0,
+        ],
+        colors="black",
+        linewidths=2,
+    )
 
-contours = ax.contour(
-    X, Y, Z, levels=[0.0001, 0.001, 0.01, 0.1], colors="black", linewidths=1
-)
-ax.clabel(contours, inline=True, fontsize=8)
-contours = ax.contour(
-    X,
-    Y,
-    Z,
-    levels=[
-        0.9999,
-    ],
-    colors="black",
-    linestyles="dashed",
-)
+    cs = ax.contourf(
+        X,
+        Y,
+        Z,
+        levels=[0.0, 0.0001, 0.001, 0.01, 0.1, 0.9999, 2.0],
+        colors=["#ccf", "#dfd", "#cfc", "#bfb", "#afa", "#9f9", "#fcc"],
+        extend="min",
+    )
 
-contours = ax.contour(
-    X,
-    Y,
-    Z,
-    levels=[
-        0.0,
-    ],
-    colors="black",
-    linewidths=2,
-)
+    plot_guest_molecules(ax=ax, gases=gases)
 
-cs = ax.contourf(
-    X,
-    Y,
-    Z,
-    levels=[0.0, 0.0001, 0.001, 0.01, 0.1, 0.9999, 2.0],
-    colors=["#ccf", "#dfd", "#cfc", "#bfb", "#afa", "#9f9", "#fcc"],
-    extend="min",
-)
-
-plot_guest_molecules(ax=ax, gases=gases)
-
-ax.annotate(
-    "(a) X + Q",  # this is the text
-    xy=(0.8, 0.92),  # these are the coordinates to position the label
-    xycoords="axes fraction",
-    fontsize=20,
-    ha="right",
-)
-
-
-# (b)
-# Search for conditions for I-II-III-I transition when mixed with Me.
-ax = axes[1]
-guest = "Methane"
-ax.set_xlabel(r"$\sigma_g / \AA$")
-ax.set_xlim(3.45, 5.1)
-ax.set_ylim(120.0, 600)
-x = np.linspace(3.45, 5.1, xyticks * 2)
-y = np.linspace(120.0, 600, xyticks * 2)
-X, Y = np.meshgrid(x, y)
-Z = np.zeros_like(X)
-Z2 = np.zeros_like(X)
-for ix, sig in enumerate(x):
-    for iy, epsK in enumerate(y):
-        inter2 = LJ(sig=(tip4pice.sig + sig) / 2, epsK=(tip4pice.epsK * epsK) ** 0.5)
-        phases, lastphase = calculate_mixed_clathrate_phase(
-            inter[guest], inter2, beta, pressure, composition_ratios=composition_ratios
-        )
-        Z[iy, ix] = len(set(phases))
-        if lastphase == "CS1":
-            z = 1
-        elif lastphase == "CS2":
-            z = 2
-        else:
-            z = 3
-        Z2[iy, ix] = z
-        # mark(sig, epsK, set(phases), lastphase, ax=ax)
-plot_guest_molecules(ax=ax, gases=gases)
-plot_phase_contours(ax, X, Y, Z, Z2)
-ax.annotate(
-    "(b) Me + X",  # this is the text
-    xy=(0.6, 0.92),  # these are the coordinates to position the label
-    xycoords="axes fraction",
-    fontsize=20,
-    ha="right",
-)
+    ax.annotate(
+        "(a) X + Q",  # this is the text
+        xy=(0.8, 0.92),  # these are the coordinates to position the label
+        xycoords="axes fraction",
+        fontsize=20,
+        ha="right",
+    )
 
 
-# (c)
-# Search for conditions for I-II-III-I transition when mixed with Xe.
-ax = axes[2]
-markers = {1: "o", 2: "+", 3: "^"}
-guest = "Xe"
-ax.set_xlabel(r"$\sigma_g / \AA$")
-# ax.set_ylabel(r"\epsilon_g / K")
-ax.set_xlim(4.55, 5.05)
-ax.set_ylim(120.0, 600)
-x = np.linspace(4.55, 5.05, xyticks * 2)
-y = np.linspace(120.0, 600, xyticks * 2)
-X, Y = np.meshgrid(x, y)
-Z = np.zeros_like(X)
-Z2 = np.zeros_like(X)
-for ix, sig in enumerate(x):
-    for iy, epsK in enumerate(y):
-        inter2 = LJ(sig=(tip4pice.sig + sig) / 2, epsK=(tip4pice.epsK * epsK) ** 0.5)
-        phases, lastphase = calculate_mixed_clathrate_phase(
-            inter[guest], inter2, beta, pressure, composition_ratios=composition_ratios
-        )
-        # mark(sig, epsK, set(phases), lastphase, ax=ax)
-        Z[iy, ix] = len(set(phases))
-        if lastphase == "CS1":
-            z = 1
-        elif lastphase == "CS2":
-            z = 2
-        else:
-            z = 3
-        Z2[iy, ix] = z
-        # mark(sig, epsK, set(phases), lastphase, ax=ax)
-# im = ax.imshow(Z, # interpolation='bilinear', origin='lower',
-#                cmap=cm.gray, extent=(4.55, 5.05, 120.0, 600.0))
+def plot_phase_diagram_Figure4b(
+    ax,
+    temperatures,
+    pressure,
+    beta,
+    mu_e,
+    structures,
+    inter,
+    tip4pice,
+    gases,
+    composition_ratios,
+    xyticks,
+):
+    # (b)
+    # Search for conditions for I-II-III-I transition when mixed with Me.
+    guest = "Methane"
+    ax.set_xlabel(r"$\sigma_g / \AA$")
+    ax.set_xlim(3.45, 5.1)
+    ax.set_ylim(120.0, 600)
+    x = np.linspace(3.45, 5.1, xyticks * 2)
+    y = np.linspace(120.0, 600, xyticks * 2)
+    X, Y = np.meshgrid(x, y)
+    Z = np.zeros_like(X)
+    Z2 = np.zeros_like(X)
+    for ix, sig in enumerate(x):
+        for iy, epsK in enumerate(y):
+            inter2 = LJ(
+                sig=(tip4pice.sig + sig) / 2, epsK=(tip4pice.epsK * epsK) ** 0.5
+            )
+            phases, lastphase = calculate_mixed_clathrate_phase(
+                inter[guest],
+                inter2,
+                beta,
+                pressure,
+                temperatures,
+                mu_e,
+                structures,
+                composition_ratios=composition_ratios,
+            )
+            Z[iy, ix] = len(set(phases))
+            if lastphase == "CS1":
+                z = 1
+            elif lastphase == "CS2":
+                z = 2
+            else:
+                z = 3
+            Z2[iy, ix] = z
+    plot_guest_molecules(ax=ax, gases=gases)
+    plot_phase_contours(ax, X, Y, Z, Z2)
+    ax.annotate(
+        "(b) Me + X",  # this is the text
+        xy=(0.6, 0.92),  # these are the coordinates to position the label
+        xycoords="axes fraction",
+        fontsize=20,
+        ha="right",
+    )
 
 
-plot_guest_molecules(ax=ax, gases=gases)
-plot_phase_contours(ax, X, Y, Z, Z2)
-ax.annotate(
-    "(c) Xe + X",  # this is the text
-    xy=(0.4, 0.92),  # these are the coordinates to position the label
-    xycoords="axes fraction",
-    fontsize=20,
-    ha="right",
-)
+def plot_phase_diagram_Figure4c(
+    ax,
+    temperatures,
+    pressure,
+    beta,
+    mu_e,
+    structures,
+    inter,
+    tip4pice,
+    gases,
+    composition_ratios,
+    xyticks,
+):
+    # (c)
+    # Search for conditions for I-II-III-I transition when mixed with Xe.
+    guest = "Xe"
+    ax.set_xlabel(r"$\sigma_g / \AA$")
+    ax.set_xlim(4.55, 5.05)
+    ax.set_ylim(120.0, 600)
+    x = np.linspace(4.55, 5.05, xyticks * 2)
+    y = np.linspace(120.0, 600, xyticks * 2)
+    X, Y = np.meshgrid(x, y)
+    Z = np.zeros_like(X)
+    Z2 = np.zeros_like(X)
+    for ix, sig in enumerate(x):
+        for iy, epsK in enumerate(y):
+            inter2 = LJ(
+                sig=(tip4pice.sig + sig) / 2, epsK=(tip4pice.epsK * epsK) ** 0.5
+            )
+            phases, lastphase = calculate_mixed_clathrate_phase(
+                inter[guest],
+                inter2,
+                beta,
+                pressure,
+                temperatures,
+                mu_e,
+                structures,
+                composition_ratios=composition_ratios,
+            )
+            Z[iy, ix] = len(set(phases))
+            if lastphase == "CS1":
+                z = 1
+            elif lastphase == "CS2":
+                z = 2
+            else:
+                z = 3
+            Z2[iy, ix] = z
+    plot_guest_molecules(ax=ax, gases=gases)
+    plot_phase_contours(ax, X, Y, Z, Z2)
+    ax.annotate(
+        "(c) Xe + X",  # this is the text
+        xy=(0.4, 0.92),  # these are the coordinates to position the label
+        xycoords="axes fraction",
+        fontsize=20,
+        ha="right",
+    )
 
 
-plt.tight_layout()
+if __name__ == "__main__":
+    # User variables
+    pressure = 101325.00 * 50  # Pa
+    temperatures = 273.15
+    beta = 1.0 / (NkB * temperatures)
 
-plt.show()
-fig.savefig("Figure4.pdf")
-fig.savefig("Figure4.png")
+    LJ = namedtuple("LJ", ["sig", "epsK"])
+
+    structures = ["CS2", "CS1", "HS1", "TS1"]
+
+    ####### chemical potential of gas ######################################
+    stericterm = chempot.molecular_chemical_potential_corrections(
+        temperatures, mass=1.0, symmetry_number=1, moment_of_inertia=(0, 0, 0)
+    )
+
+    mu_g = (
+        chempot.ideal_gas_chemical_potential(temperatures, pressure)
+        + chempot.phase_space_integration_correction(temperatures, degrees_of_freedom=0)
+        + stericterm
+    )
+
+    ####### structure-dependent terms ######################################
+
+    mu_e = crystals.mu_e
+
+    composition_ratios = np.concatenate(
+        [np.array([0.0]), np.logspace(-4, 0.0, 300)]
+    )  # 1e-4 .. 1e0
+
+    plt.rcParams["font.size"] = 14
+    plt.rcParams["font.family"] = "sans-serif"
+
+    fig, axes = plt.subplots(
+        nrows=1, ncols=3, figsize=(12, 5), sharey=True, gridspec_kw={"wspace": 0}
+    )
+
+    xyticks = 40
+
+    plot_phase_diagram_Figure4a(
+        axes[0],
+        temperatures,
+        pressure,
+        beta,
+        mu_e,
+        structures,
+        inter,
+        tip4pice,
+        gases,
+        composition_ratios,
+        xyticks,
+    )
+    plot_phase_diagram_Figure4b(
+        axes[1],
+        temperatures,
+        pressure,
+        beta,
+        mu_e,
+        structures,
+        inter,
+        tip4pice,
+        gases,
+        composition_ratios,
+        xyticks,
+    )
+    plot_phase_diagram_Figure4c(
+        axes[2],
+        temperatures,
+        pressure,
+        beta,
+        mu_e,
+        structures,
+        inter,
+        tip4pice,
+        gases,
+        composition_ratios,
+        xyticks,
+    )
+    plt.tight_layout()
+
+    plt.show()
+    fig.savefig("Figure4.pdf")
+    fig.savefig("Figure4.png")
